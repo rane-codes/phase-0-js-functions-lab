012968c8b0e051124f7649b42a76cf5f94a79177
rane-codes <ranemuithya@gmail.com>
2026-09-24 19:39:59 +0300

    final

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
+function calculateTax(amount) {
+  return amount * 0.1;
+}
 
+function convertToUpperCase(text) {
+  return text.toUpperCase();
+}
 
+function findMaximum(num1, num2) {
+  return Math.max(num1, num2);
+}
 
+function isPalindrome(word) {
+  const normalizedWord = word.toLowerCase();
+  return normalizedWord === normalizedWord.split('').reverse().join('');
+}
 
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+  const discountAmount = (originalPrice * discountPercentage) / 100;
+  return originalPrice - discountAmount;
+}
 
-// This is required for the test to function properly  
-module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
+console.log(calculateTax(50)); // returns: 5
+console.log(convertToUpperCase('Hello')); // returns: "HELLO"
+console.log(findMaximum(14, 25)); // returns: 25
+console.log(isPalindrome('Racecar')); // returns: true
+console.log(calculateDiscountedPrice(100, 20)); // returns: 80
+
+module.exports = {
+  calculateTax,
+  convertToUpperCase,
+  findMaximum,
+  isPalindrome,
+  calculateDiscountedPrice,
+};
